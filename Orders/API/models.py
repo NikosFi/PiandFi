f64f645eb835f520bc75d017e92c8c2b2bdd3e1c
Nick Filippopoulos <61012515+NikosFi@users.noreply.github.com>
2020-05-11 23:10:14 +0100

    that's going to master

--- Orders/API/models.py
+++ Orders/API/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class order(models.Model):
-    id = models.IntegerField(max_length=60, unique=True)
-    api_version = models.IntegerField(max_length=60, unique=False)
-    type = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
-    missed_reason = models.CharField(max_length=100, null=True)
-    persons = models.IntegerField
+# class order(models.Model):
+#     id = models.IntegerField(max_length=60, unique=True)
+#     api_version = models.IntegerField(max_length=60, unique=False)
+#     type = models.CharField(max_length=200)
+#     status = models.CharField(max_length=200)
+#     missed_reason = models.CharField(max_length=100, null=True)
+#     persons = models.IntegerField
 
 
     #wanna work now?
